84124b4d4891c846455371de5ab437ef304b86c4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-01 09:20:35 +0500

    Update Adding New cs file

---
+++ TestXam/TestXam/SlidingPanel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestXam
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SlidingPanel : ContentPage
+    {
+        public SlidingPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
