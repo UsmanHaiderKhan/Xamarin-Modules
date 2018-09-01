0afa8404ec8134efde2bb053ab996bffc5b44a17
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-01 11:16:43 +0500

    Update the Resource of the Application

--- TestXam/TestXam.Android/Resources/Resource.designer.cs
+++ TestXam/TestXam.Android/Resources/Resource.designer.cs
@@ -2343,26 +2343,26 @@ namespace TestXam.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020146
-			public const int avd_hide_password_1 = 2130837830;
-			
 			// aapt resource value: 0x7f020147
-			public const int avd_hide_password_2 = 2130837831;
+			public const int avd_hide_password_1 = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int avd_hide_password_3 = 2130837832;
+			public const int avd_hide_password_2 = 2130837832;
+			
+			// aapt resource value: 0x7f020149
+			public const int avd_hide_password_3 = 2130837833;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020149
-			public const int avd_show_password_1 = 2130837833;
-			
 			// aapt resource value: 0x7f02014a
-			public const int avd_show_password_2 = 2130837834;
+			public const int avd_show_password_1 = 2130837834;
 			
 			// aapt resource value: 0x7f02014b
-			public const int avd_show_password_3 = 2130837835;
+			public const int avd_show_password_2 = 2130837835;
+			
+			// aapt resource value: 0x7f02014c
+			public const int avd_show_password_3 = 2130837836;
 			
 			// aapt resource value: 0x7f020055
 			public const int call = 2130837589;
@@ -3036,11 +3036,11 @@ namespace TestXam.Droid
 			// aapt resource value: 0x7f020134
 			public const int notification_icon_background = 2130837812;
 			
-			// aapt resource value: 0x7f020144
-			public const int notification_template_icon_bg = 2130837828;
-			
 			// aapt resource value: 0x7f020145
-			public const int notification_template_icon_low_bg = 2130837829;
+			public const int notification_template_icon_bg = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int notification_template_icon_low_bg = 2130837830;
 			
 			// aapt resource value: 0x7f020135
 			public const int notification_tile_bg = 2130837813;
@@ -3049,43 +3049,46 @@ namespace TestXam.Droid
 			public const int notify_panel_notification_icon_bg = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int power = 2130837815;
+			public const int pnel = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int pp = 2130837816;
+			public const int power = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int screen = 2130837817;
+			public const int pp = 2130837817;
 			
 			// aapt resource value: 0x7f02013a
-			public const int serach = 2130837818;
+			public const int screen = 2130837818;
 			
 			// aapt resource value: 0x7f02013b
-			public const int setting = 2130837819;
+			public const int serach = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int settings = 2130837820;
+			public const int setting = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int thour = 2130837821;
+			public const int settings = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int tooltip_frame_dark = 2130837822;
+			public const int thour = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int tooltip_frame_light = 2130837823;
+			public const int tooltip_frame_dark = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int twitter = 2130837824;
+			public const int tooltip_frame_light = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int Userl = 2130837825;
+			public const int twitter = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int Users = 2130837826;
+			public const int Userl = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int usman = 2130837827;
+			public const int Users = 2130837827;
+			
+			// aapt resource value: 0x7f020144
+			public const int usman = 2130837828;
 			
 			static Drawable()
 			{
